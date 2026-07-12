cb0e10242632914a5d41cfb7835b0681cf907a28
MD Arman khan <mdarmankhan6252@gmail.com>
2026-07-12 19:48:55 +0600

    added mongodb connected

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,20 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import connectDB from './app/config/db.js';
 
 dotenv.config();
 const app = express();
-
 const port = 5000;
 
+//database connected
+connectDB();
+
+//middlewares
 app.use(express.json());
 app.use(cors())
 
+// application routes
 app.get('/', (req: Request, res: Response) => {
     res.status(200).send({
         success: true,
